3e6ee9c25445cc3893de557b177b93c10a88a5d0
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-08-12 05:41:27 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,14 +1,10 @@
 # code here!
 class School
-  attr_reader :school
+  attr_reader :school :roster
   
  def initialize(school)
   @school = school
   @roster = {}
- end
- 
- def roster 
-   @roster
  end
 
 def add_student(grade, name)
